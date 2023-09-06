c88fd8a845b9cd9c8d718dd2f6c34dd486daa626
Kamente <justinm48000@gmail.com>
2023-09-06 19:59:00 +0300

    comment

--- sequences.py
+++ sequences.py
@@ -1,6 +1,5 @@
 def remove_duplicates(sequence):
     input_list = []
-
     encountered = set()
 
     for element in sequence:
@@ -8,10 +7,9 @@ def remove_duplicates(sequence):
         if element not in encountered:
             input_list.append(element)
             encountered.add(element)
-
     return input_list
 
-
+# test
 input_sequence = [1,1,1,1,2,2,2,2]
 result = remove_duplicates(input_sequence)
 print(result)
